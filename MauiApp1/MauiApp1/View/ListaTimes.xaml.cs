8e0ffe71f382f762fd39abc2356743e339a124ff
matheus <matheusdacruzsouza1998@gmail.com>
2023-10-16 12:54:56 -0300

    subindo att

--- MauiApp1/MauiApp1/View/ListaTimes.xaml.cs
+++ MauiApp1/MauiApp1/View/ListaTimes.xaml.cs
@@ -7,7 +7,9 @@ public partial class ListaTimes : ContentPage
 	public ListaTimes()
 	{
 		InitializeComponent();
-	}
+        AddTeamsToLayout();
+
+    }
 
     private async void OnGenerateTeamsClicked(object sender, EventArgs e)
     {
@@ -15,7 +17,7 @@ public partial class ListaTimes : ContentPage
         List<TimeJogadores> lista = new List<TimeJogadores>();
         lista = GenerateTeamsData();
 
-        TeamsCollectionView.ItemsSource = lista;
+        //TeamsCollectionView.ItemsSource = lista;
     }
 
     private List<TimeJogadores> GenerateTeamsData()
@@ -43,5 +45,59 @@ public partial class ListaTimes : ContentPage
             }
             
         };
+    }
+
+    private void AddTeamsToLayout()
+    {
+        var times = new List<TimeJogadores>
+        {
+            new TimeJogadores
+            {
+                nomeDoTime = "Time A",
+                nomeDosJogadores = new List<string> { "jose", "john", "matheus", "guilherme", "ruan" }
+            },
+            new TimeJogadores
+            {
+                nomeDoTime = "Time B",
+                nomeDosJogadores = new List<string> { "lucas", "moises", "maria", "otaci", "junio" }
+            },
+            new TimeJogadores
+            {
+                nomeDoTime = "Time C",
+                nomeDosJogadores = new List<string> { "lucas", "moises", "maria", "otaci", "junio" }
+            }
+        };
+
+        foreach (var time in times)
+        {
+            var stackLayout = new StackLayout
+            {
+                Spacing = 10
+                             // Espaçamento vertical dentro de cada time
+            };
+
+            var nomeDoTimeLabel = new Label
+            {
+                Text = time.nomeDoTime,
+                FontAttributes = FontAttributes.Bold,
+                FontSize = 20
+                
+            };
+
+            stackLayout.Children.Add(nomeDoTimeLabel);
+
+            foreach (var jogador in time.nomeDosJogadores)
+            {
+                var jogadorLabel = new Label
+                {
+                    Text = jogador,
+                  
+
+                };
+                stackLayout.Children.Add(jogadorLabel);
+            }
+
+            TeamsFlexLayout.Children.Add(stackLayout);
+        }
     }
 }
