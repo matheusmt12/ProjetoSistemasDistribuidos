0efdc014949c7241008d8718afb109f6a1d33390
junio10 <junio.js977@gmail.com>
2023-10-15 23:48:26 -0300

    refactor: adicionando mudancas no retorno

---
+++ APIPelada/Core/DTO/TimeJogadoreDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DTO
+{
+    public class TimeJogadoreDTO
+    {
+        public string? NomeDoTime { get; set; }
+        public List<string>? NomeDosJogadores { get; set; } = new List<string>();
+    }
+}
